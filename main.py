a622547068a304a4bd3a41ead6f272604020412d
Eduardo Sequeira <eduardoasm19@gmail.com>
2024-11-27 17:25:00 -0400

    add file data.json on get elements url op.gg

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
 import time
+import json
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -36,14 +36,51 @@ region = driver.find_element(By.CLASS_NAME, "css-r2ch24.em8s8ey1")
 region.click()
 
 WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.LINK_TEXT, "LAN"))
+    EC.presence_of_element_located(
+        (By.XPATH, "//button[contains(@class, 'css-60l9xa em8s8ey3')]/span[text()='LAN']"))
 )
 
-buttonLan = driver.find_element(By.LINK_TEXT, "LAN")
+buttonLan = driver.find_element(
+    By.XPATH, "//button[contains(@class, 'css-60l9xa em8s8ey3')]/span[text() = 'LAN']")
 buttonLan.click()
 
 input_element.clear()
-input_element.send_keys("lalalxd", Keys.ENTER)
+input_element.send_keys("shir4k0", Keys.ENTER)
+
+disabledButton = "DISABLE css-1r09es5 e17xj3f90"
+enabledButton = "IDLE css-1ki6o6m e17xj3f90"
+
+WebDriverWait(driver, 5).until(
+    EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, ".IDLE.css-1ki6o6m.e17xj3f90")
+    )
+)
+
+enabledButton = driver.find_element(
+    By.CSS_SELECTOR, ".IDLE.css-1ki6o6m.e17xj3f90")
+enabledButton.click()
+
+WebDriverWait(driver, 7).until(
+    EC.presence_of_element_located(
+        (By.CSS_SELECTOR, ".DISABLE.css-1r09es5.e17xj3f90"))
+)
+
+WebDriverWait(driver, 3).until(
+    EC.presence_of_element_located(
+        (By.CLASS_NAME, "tier"))
+)
+
+tier = driver.find_element(By.CLASS_NAME, "tier").text
+lp = driver.find_element(By.CLASS_NAME, "lp").text
+
+data = {
+    "tier": tier,
+    "lp": lp,
+}
+
+with open("data.json", "w") as file:
+    json.dump(data, file, indent=4)
+
 
 time.sleep(30)
 
